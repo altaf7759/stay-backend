bc6f1731470ebd5ced68a727ce5dc33a6574b3ff
Altaf Raja <altaf.1040216@gmail.com>
2025-02-24 22:32:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -46,6 +46,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
+});
 
 // Google login route
 app.post("/login", async (req, res) => {
