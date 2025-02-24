5714d5aa5814f79409946962dbf09ba2e7743755
Altaf Raja <altafraja7369892024.com>
2025-02-24 19:29:32 +0530

    cors

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({ origin: "https://stay-backend.onrender.com/" }));
+app.use(cors());
 app.use((req, res, next) => {
   res.setHeader("Cross-Origin-Opener-Policy", "unsafe-none");
   res.setHeader("Cross-Origin-Embedder-Policy", "unsafe-none");
